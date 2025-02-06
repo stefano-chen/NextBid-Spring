6006d666e32528dbe97c17c17b409f0427d2a298
stefano-chen <stefanochen01@gmail.com>
2025-02-06 13:52:38 +0100

    initial commit

---
+++ src/main/java/com/stefano/nextbid/NextbidApplication.java
@@ -0,0 +1,13 @@
+package com.stefano.nextbid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextbidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextbidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stefano/nextbid/NextbidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefano.nextbid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextbidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
