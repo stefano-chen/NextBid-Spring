276acaac05dd46ec6ce724dc5a793b1eaeb12b81
stefano-chen <stefanochen01@gmail.com>
2025-02-08 19:37:01 +0100

    UserMapper unit test

--- src/main/java/com/stefano/nextbid/service/UserMapper.java
+++ src/main/java/com/stefano/nextbid/service/UserMapper.java
@@ -9,11 +9,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserMapper {
     public User mapToUser(SignupBody body) {
+        if (body == null)
+            return null;
         return new User(body.username(), body.name(), body.surname(), "", body.password());
     }
 
 
     public UserDTO mapToUserDTO(User user) {
+        if (user == null)
+            return null;
         return new UserDTO(user.getId(), user.getUsername(), user.getName(), user.getSurname(), user.getBio(), user.getCreatedAt());
     }
 }
