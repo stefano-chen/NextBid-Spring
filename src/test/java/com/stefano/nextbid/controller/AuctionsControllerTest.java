88afd21e6f2c30b68d900ef57023459790dfa09b
stefano-chen <stefanochen01@gmail.com>
2025-02-11 11:03:26 +0100

    test case for get all auctions endpoint

--- src/test/java/com/stefano/nextbid/controller/AuctionsControllerTest.java
+++ src/test/java/com/stefano/nextbid/controller/AuctionsControllerTest.java
@@ -6,6 +6,7 @@ import com.stefano.nextbid.entity.User;
 import com.stefano.nextbid.exceptions.InvalidAuctionDataException;
 import com.stefano.nextbid.service.AuctionMapper;
 import com.stefano.nextbid.service.AuctionService;
+import com.stefano.nextbid.service.AuthService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +16,15 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -32,6 +36,8 @@ class AuctionsControllerTest {
 
     @MockitoBean
     private AuctionService auctionService;
+    @Autowired
+    private AuthService authService;
 
 
     @Test
@@ -60,5 +66,47 @@ class AuctionsControllerTest {
         AuctionDTO createdAuction = new AuctionDTO(1,"auction", "description", 10.0,  Instant.parse("3026-07-20T13:05:30.00Z"), Instant.now(), new User(1), null);
         when(auctionService.createAuction(auctionBody)).thenReturn(createdAuction);
         this.mockMvc.perform(post("/api/auctions").content(body).contentType("application/json")).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    void getAllAuctionsWhenThereAreNoAuctionsShouldReturnEmptyList() throws Exception {
+        assertTrue(this.mockMvc.perform(get("/api/auctions")).andDo(print()).andExpect(status().isOk()).andReturn().getResponse().getContentAsString().equals("[]"));
+    }
+
+    @Test
+    void getAllAuctionsWhenThereAreAuctionsShouldReturnList() throws Exception {
+
+        List<AuctionDTO> auctions = List.of(new AuctionDTO(1,"title1", "description1", 10.0, Instant.now().plus(1, ChronoUnit.DAYS), Instant.now(), new User(1), null),
+                new AuctionDTO(2,"title2", "description2", 10.0, Instant.now().plus(1, ChronoUnit.DAYS), Instant.now(), new User(2), null));
+
+        when(authService.getAllAuctions("")).thenReturn(auctions);
+
+        this.mockMvc.perform(get("/api/auctions")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].title").value("title1"))
+                .andExpect(jsonPath("$[1].title").value("title2"));
+    }
+
+    @Test
+    void getAllAuctionsWhenThereAreNoMatchShouldReturnEmptyList() throws Exception {
+
+        List<AuctionDTO> auctions = List.of(new AuctionDTO(1,"title1", "description1", 10.0, Instant.now().plus(1, ChronoUnit.DAYS), Instant.now(), new User(1), null),
+                new AuctionDTO(2,"title2", "description2", 10.0, Instant.now().plus(1, ChronoUnit.DAYS), Instant.now(), new User(2), null));
+
+        when(authService.getAllAuctions("title3")).thenReturn(List.of());
+
+        assertTrue(this.mockMvc.perform(get("/api/auctions")).andDo(print()).andExpect(status().isOk()).andReturn().getResponse().getContentAsString().equals("[]"));
+    }
+
+    @Test
+    void getAllAuctionsWhenThereAreMatchShouldReturnList() throws Exception {
+
+        List<AuctionDTO> auctions = List.of(new AuctionDTO(1,"title1", "description1", 10.0, Instant.now().plus(1, ChronoUnit.DAYS), Instant.now(), new User(1), null),
+                new AuctionDTO(2,"title2", "description2", 10.0, Instant.now().plus(1, ChronoUnit.DAYS), Instant.now(), new User(2), null));
+
+        when(authService.getAllAuctions("title1")).thenReturn(auctions);
+
+        this.mockMvc.perform(get("/api/auctions")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].title").value("title1"))
+                .andExpect(jsonPath("$[1]").doesNotExist());
     }
 }
